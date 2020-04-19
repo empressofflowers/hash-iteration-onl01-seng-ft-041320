570466202b31eadb54eba0dd24c93e82afb89636
Devone Austin <devone.austin@gmail.com>
2020-04-19 04:06:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,8 +11,8 @@
  end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    if age != :age[12]
+  birthday_kids.select do |kids_name, age|
+    if age > 12
       puts "You are too old for this."
     else age == :age[12]
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
